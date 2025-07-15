a6bd16f9af5b547919118683c54bfe3f277052c3
schaeff <thibaut@powdrlabs.com>
2025-07-15 18:04:21 +0200

    implement instr

--- crates/core/machine/src/autoprecompiles/instruction.rs
+++ crates/core/machine/src/autoprecompiles/instruction.rs
@@ -1,5 +1,6 @@
 use powdr_autoprecompiles::blocks::Instruction;
 use serde::{Deserialize, Serialize};
+use slop_algebra::AbstractField;
 use slop_baby_bear::BabyBear;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -7,12 +8,21 @@ pub struct Sp1Instruction(pub sp1_core_executor::Instruction);
 
 impl Instruction<BabyBear> for Sp1Instruction {
     fn opcode(&self) -> usize {
-        todo!()
+        unimplemented!()
     }
 
     fn into_symbolic_instruction(
         self,
     ) -> powdr_autoprecompiles::SymbolicInstructionStatement<BabyBear> {
-        todo!()
+        powdr_autoprecompiles::SymbolicInstructionStatement {
+            opcode: (self.0.opcode as u8) as usize,
+            args: vec![
+                BabyBear::from_canonical_u32(self.0.op_a as u32),
+                BabyBear::from_canonical_u32(self.0.op_b as u32),
+                BabyBear::from_canonical_u32(self.0.op_c as u32),
+                BabyBear::from_canonical_u32(self.0.imm_b as u32),
+                BabyBear::from_canonical_u32(self.0.imm_c as u32),
+            ],
+        }
     }
 }
