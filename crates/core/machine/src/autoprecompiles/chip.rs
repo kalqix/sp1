7517633d773356f7dab3a4ffa24b66da7113c49f
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2025-08-11 19:48:00 +0800

    implement eval for powdr chip, but can't unify concrete adapter field (babybear) vs generic AirBuilder::F

--- crates/core/machine/src/autoprecompiles/chip.rs
+++ crates/core/machine/src/autoprecompiles/chip.rs
@@ -1,10 +1,15 @@
 use std::{borrow::Borrow, collections::BTreeMap, sync::Arc};
 
 use itertools::Itertools;
-use powdr_autoprecompiles::adapter::AdapterApc;
+use powdr_autoprecompiles::{
+    adapter::AdapterApc,
+    expression::{AlgebraicExpression, AlgebraicReference},
+};
+use powdr_expression::{AlgebraicBinaryOperator, AlgebraicUnaryOperator};
 use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
-use slop_air::{Air, BaseAir, PairBuilder};
+use slop_air::{Air, AirBuilder, BaseAir, PairBuilder};
 use slop_algebra::PrimeField32;
+use slop_baby_bear::BabyBear;
 use slop_matrix::{dense::RowMajorMatrix, Matrix};
 use sp1_core_executor::{ExecutionRecord, Program};
 use sp1_stark::{
@@ -44,11 +49,28 @@ pub struct ApcChip<F: PrimeField32> {
     cached_apc: CachedApc,
     /// A machine to generate traces for the APC.
     machine: Machine<F, RiscvAir<F>>,
+    /// A map from poly ID to column index.
+    column_index_by_poly_id: BTreeMap<u64, usize>,
+    /// The columns of the APC.
+    columns: Vec<AlgebraicReference>,
 }
 
 impl<F: PrimeField32> ApcChip<F> {
     pub fn new(apc: Arc<AdapterApc<Sp1ApcAdapter>>, id: usize) -> Self {
-        Self { id: id as u64, cached_apc: apc.into(), machine: RiscvAir::machine() }
+        let (column_index_by_poly_id, columns): (BTreeMap<_, _>, Vec<_>) = apc
+            .machine()
+            .main_columns()
+            .enumerate()
+            .map(|(index, c)| ((c.id, index), c.clone()))
+            .unzip();
+
+        Self {
+            id: id as u64,
+            cached_apc: apc.into(),
+            machine: RiscvAir::machine(),
+            column_index_by_poly_id,
+            columns,
+        }
     }
 
     pub fn apc(&self) -> &Arc<AdapterApc<Sp1ApcAdapter>> {
@@ -184,13 +206,63 @@ where
     AB::F: PrimeField32,
 {
     fn eval(&self, builder: &mut AB) {
-        // TODO: Implement the evaluation logic for the APC air
-        // For now, boolean constrain a single column
         let main = builder.main();
+        let witnesses = main.row_slice(0);
+
+        let witness_values: BTreeMap<u64, AB::Var> =
+            self.columns.iter().map(|c| c.id).zip_eq(witnesses.iter().cloned()).collect();
+
+        let witness_evaluator = WitnessEvaluator::<AB>::new(&witness_values);
+
+        for constraint in &self.cached_apc.apc.machine().constraints {
+            let e = witness_evaluator.eval_expr(&constraint.expr);
+            builder.assert_zero(e);
+        }
+
+        // Add a dummy bus interaction, otherwise `/stark/src/logup_gkr/execution.rs:237:30` fails
         let col = main.row_slice(0);
         let col: &AB::Var = col[0].borrow();
-        builder.assert_bool(*col);
-        // Add a dummy bus interaction, otherwise `/stark/src/logup_gkr/execution.rs:237:30` fails
         builder.send_byte(*col, *col, *col, *col, *col);
+    }
+}
+
+pub struct WitnessEvaluator<'a, AB: AirBuilder> {
+    pub witness: &'a BTreeMap<u64, AB::Var>,
+}
+
+impl<'a, AB: AirBuilder> WitnessEvaluator<'a, AB> {
+    pub fn new(witness: &'a BTreeMap<u64, AB::Var>) -> Self {
+        Self { witness }
+    }
+}
+
+impl<AB: AirBuilder> WitnessEvaluator<'_, AB> {
+    fn eval_const(&self, c: BabyBear) -> AB::Expr {
+        c.into()
+    }
+
+    fn eval_var(&self, symbolic_var: AlgebraicReference) -> AB::Expr {
+        (*self.witness.get(&(symbolic_var.id as u64)).unwrap()).into()
+    }
+
+    fn eval_expr(&self, algebraic_expr: &AlgebraicExpression<BabyBear>) -> AB::Expr {
+        match algebraic_expr {
+            AlgebraicExpression::Number(n) => self.eval_const(*n),
+            AlgebraicExpression::BinaryOperation(binary) => match binary.op {
+                AlgebraicBinaryOperator::Add => {
+                    self.eval_expr(&binary.left) + self.eval_expr(&binary.right)
+                }
+                AlgebraicBinaryOperator::Sub => {
+                    self.eval_expr(&binary.left) - self.eval_expr(&binary.right)
+                }
+                AlgebraicBinaryOperator::Mul => {
+                    self.eval_expr(&binary.left) * self.eval_expr(&binary.right)
+                }
+            },
+            AlgebraicExpression::UnaryOperation(unary) => match unary.op {
+                AlgebraicUnaryOperator::Minus => -self.eval_expr(&unary.expr),
+            },
+            AlgebraicExpression::Reference(var) => self.eval_var(var.clone()),
+        }
     }
 }
