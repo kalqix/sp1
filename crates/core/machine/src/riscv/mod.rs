14f0f83d3e36d6caf2f8bef0f166ceb2f51de329
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2025-08-19 22:57:22 +0800

    remove a test

--- crates/core/machine/src/riscv/mod.rs
+++ crates/core/machine/src/riscv/mod.rs
@@ -1323,31 +1323,6 @@ pub mod tests {
             .unwrap();
     }
 
-    #[tokio::test]
-    async fn test_add_apc_prove_read_same() {
-        setup_logger();
-        let mut instructions = vec![
-            Instruction::new(Opcode::ADDI, 25, 0, 5, false, true),
-            // Instruction::new(Opcode::ADDI, 25, 0, 7, false, true),
-            Instruction::new(Opcode::ADDI, 25, 0, 7, false, true),
-            // Instruction::new(Opcode::ADDI, 25, 0, 5, false, true),
-            // Instruction::new(Opcode::ADDI, 25, 0, 7, false, true),
-        ];
-
-        add_halt(&mut instructions);
-        let apc_ranges = vec![(1, 2)];
-        let program = Program::new(instructions, 0, 0);
-        // TODO: The API is not great here, we should be able to pass the full apcs (not just the
-        // ranges) to the program Then in `run_test` the apcs can be passed to the prover,
-        // instead of passing them here to `run_test_with_apcs`
-        let apcs = create_apcs(&program, &apc_ranges);
-        let program = program.with_apcs(&apc_ranges);
-        let stdin = SP1Stdin::new();
-        crate::utils::run_test_with_machine(program, stdin, RiscvAirWithApcs::machine(apcs))
-            .await
-            .unwrap();
-    }
-
     #[test]
     fn test_chips_main_width_interaction_ratio() {
         let chips = RiscvAir::<BabyBear>::chips();
