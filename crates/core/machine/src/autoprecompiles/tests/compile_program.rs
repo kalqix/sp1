fc7d2561fed601814a6dea29322acb0578c34db4
Georg Wiese <georgwiese@gmail.com>
2025-08-26 16:46:16 +0200

    Update snapshots

--- crates/core/machine/src/autoprecompiles/tests/compile_program.rs
+++ crates/core/machine/src/autoprecompiles/tests/compile_program.rs
@@ -130,7 +130,7 @@ fn test_compile_program_keccak256_software_cell_pgo() {
         AirStats {
             main_columns: 3155,
             constraints: 502,
-            bus_interactions: 2119,
+            bus_interactions: 2019,
         }
     "#]]
     .assert_debug_eq(&apc_stats_after);
