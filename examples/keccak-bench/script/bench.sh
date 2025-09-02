9a63ad7c58ee3ae6487dedc3e9afdc7bf1a10f11
Georg Wiese <georgwiese@gmail.com>
2025-09-02 12:26:05 +0200

    Add hack to bench script

--- examples/keccak-bench/script/bench.sh
+++ examples/keccak-bench/script/bench.sh
@@ -12,5 +12,8 @@ fi
 
 name=${NUM_HASHES}_hashes_${APCS}_apcs${SUFFIX}
 
+# HACK: currently, Cargo generates a new Cargo.lock and then it doesn't compile anymore...
+git checkout ../../Cargo.lock
+
 RUST_LOG_FORMAT=json RUST_LOG=debug cargo run -r -- --num-hashes $NUM_HASHES --apcs $APCS $MANUAL_FLAG &> log_${name}.txt
 cat log_${name}.txt | ../../parse_logs.py > results_${NUM_HASHES}_hashes_${APCS}_apcs${SUFFIX}.csv
