5a79255bb88076cdd2a5588003ab33ee0b76cd40
Georg Wiese <georgwiese@gmail.com>
2025-09-02 11:28:53 +0200

    Fix parse_logs.py script

--- examples/parse_logs.py
+++ examples/parse_logs.py
@@ -5,11 +5,12 @@ TARGET_MESSAGE = "prove_shard_with_data finished"
 
 def main():
     w = csv.writer(sys.stdout)
-    w.writerow(["shard", "total_ms", "total_cells", "bus_interactions"])
+    w.writerow(["shard_index", "total_ms", "total_cells", "bus_interactions"])
 
     time_sum = 0
     cells_sum = 0
     bus_interactions_sum = 0
+    shard_index = 0
 
     for line in sys.stdin:
         line = line.strip()
@@ -24,12 +25,10 @@ def main():
         if obj.get("message") != TARGET_MESSAGE:
             continue
 
-        shard = obj.get("shard")
         total_ms = obj.get("total_ms")
         total_cells = obj.get("total_cells")
         bus_interactions = obj.get("bus_interactions")
 
-        shard_num = int(shard)
         total_ms_num = int(total_ms)
         total_cells_num = int(total_cells)
         bus_interactions_num = int(bus_interactions)
@@ -38,7 +37,8 @@ def main():
         cells_sum += total_cells_num
         bus_interactions_sum += bus_interactions_num
 
-        w.writerow([shard_num, total_ms_num, total_cells_num, bus_interactions_num])
+        w.writerow([shard_index, total_ms_num, total_cells_num, bus_interactions_num])
+        shard_index += 1
 
     # Write the totals row
     w.writerow(["total", time_sum, cells_sum, bus_interactions_sum])
